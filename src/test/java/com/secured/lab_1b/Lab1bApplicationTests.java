f178958be9163b7489e83b7125d8b2904b358b32
irfanMKhan <irfanupdate@yahoo.com>
2025-04-03 20:39:15 -0500

    Update

--- src/test/java/com/secured/lab_1b/Lab1bApplicationTests.java
+++ src/test/java/com/secured/lab_1b/Lab1bApplicationTests.java
@@ -1,13 +1,5 @@
 package com.secured.lab_1b;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class Lab1bApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
